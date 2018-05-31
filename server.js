e5f92ebf6b22e2b71fd14d8b9782125c0c71ed90
Merek Smith <mereksmith@hotmail.com>
2018-05-30 21:29:37 -0600

    intial

---
+++ server.js
@@ -0,0 +1,42 @@
+// Using this template, the cheerio documentation,
+// and what you've learned in class so far, scrape a website
+// of your choice, save information from the page in a result array, and log it to the console.
+
+var cheerio = require("cheerio");
+var request = require("request");
+
+// Make a request call to grab the HTML body from the site of your choice
+request("http://www.ksl.com", function (error, response, html) {
+
+	// Load the HTML into cheerio and save it to a variable
+	// '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
+	var $ = cheerio.load(html);
+
+	// An empty array to save the data that we'll scrape
+	var results = [];
+
+	// Select each element in the HTML body from which you want information.
+	// NOTE: Cheerio selectors function similarly to jQuery's selectors,
+	// but be sure to visit the package's npm page to see how it works
+	$("div.queue_story").each(function (i, element) {
+
+		var link = $(element).children("div.headline").children("h2").children().attr("href");
+		var title = $(element).children("div.headline").children("h2").children().text();
+		var postedBy = $(element).children("div.headline").children("h4").children().text();
+		var description = $(element).children("div.headline").children("h5").text();
+		var picture = $(element).children("figure.image_box").children("a").children("picture").children("img").attr("data-srcset");
+
+		// Save these results in an object that we'll push into the results array we defined earlier
+		results.push({
+			title: title,
+			link: link,
+			postedBy: postedBy,
+			description: description,
+			picture: picture
+		});
+	});
+
+	// Log the results once you've looped through each of the elements found with cheerio
+	console.log(results);
+	console.log(results.length);
+});
